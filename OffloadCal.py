2afdbb80274537cebdcbc04d8a1f8faed0e3c6ea
nuohen <892904272@qq.com>
2022-04-24 14:48:58 +0800

    添加能量消耗与任务到达率关系代码and 任务丢弃率

---
+++ OffloadCal.py
@@ -0,0 +1,8 @@
+
+total =200
+xiezai=50
+daxiao = 10
+xiezaiEnergy = total*(daxiao/14)*1.3+xiezai*(daxiao*0.1)*10
+
+
+print(str(xiezaiEnergy))
---
+++ localCal.py
@@ -0,0 +1,7 @@
+bendi=170
+
+bendiEnergy = 5*0.775*bendi*0.9
+
+
+
+print("bendi:"+str(bendiEnergy))
